4cc31af7d36f4e8d40fb6a212bdb7fbb3e6e05f2
unknown <ashwaneeth@gmail.com>
2025-12-28 15:41:27 +0530

    JUnit

---
+++ src/test/java/com/project/journalApp/service/UserServiceTests.java
@@ -0,0 +1,19 @@
+//package com.project.journalApp.service;
+//
+//import com.project.journalApp.entity.User;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//public class UserServiceTests {
+//
+//    @Autowired
+//    private UserService userService;
+//
+//    @Test
+//    public void getByUserName(){
+//        Assertions.assertNotNull(userService.getByUserName("Chinnu"));
+//    }
+//}
