bd2e068167381b665382936b1996abe9c2d608dd
unknown <ashwaneeth@gmail.com>
2025-12-31 21:01:22 +0530

    health check

--- src/main/java/com/project/journalApp/controllers/PublicController.java
+++ src/main/java/com/project/journalApp/controllers/PublicController.java
@@ -3,10 +3,7 @@ package com.project.journalApp.controllers;
 import com.project.journalApp.entity.User;
 import com.project.journalApp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/public")
@@ -18,6 +15,11 @@ public class PublicController {
     @PostMapping("/create-user")
     public void addUser(@RequestBody User user){
         userService.saveNewUser(user);
+    }
+
+    @GetMapping("health-check")
+    public String healthCheck(){
+        return "ok";
     }
 
 }
